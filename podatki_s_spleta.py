f5fb587a978e1e0e5cca9f57a98574c44b12e714
Zala Starc <zala.starc@gmail.com>
2024-08-01 14:12:01 +0200

    Popravek funkcije izlusci_podatke_iz_posameznega_bloka, saj se je zataknilo pri velikih in malih začetnicah.

--- podatki_s_spleta.py
+++ podatki_s_spleta.py
@@ -14,17 +14,17 @@ def izlusci_podatke(niz):
     vzorec = r'<td data-label="Skupina">.*?</tr>'
     return re.findall(vzorec, niz, flags=re.DOTALL)
 
-print(len(izlusci_podatke(prenesi_html("https://zotks.si/priznanja/logika/"))))
+# print(len(izlusci_podatke(prenesi_html("https://zotks.si/priznanja/logika/"))))
 
 def izlusci_podatke_iz_posameznega_bloka(blok):
     slovar = {}
     vzorec = re.compile(
-        r'<td data-label="Skupina">(?P<Skupina>.*?)</td>.*?'
-        r'<td data-label="Tekmovalec">(?P<Tekmovalec>.*?)</td>.*?'
-        r'<td data-label="Mentor">(?P<Mentor>.*?)</td>.*?'
-        r'<td data-label="Šola">(?P<Šola>.*?)</td>.*?'
-        r'<td data-label="Točke">(?P<Točke>.*?)</td>.*?'
-        r'<td data-label="Mesto">(?P<Mesto>.*?)</td>',
+        r'<td data-label="(s||S)kupina">(?P<Skupina>.*?)</td>.*?'
+        r'<td data-label="(t||T)ekmovalec">(?P<Tekmovalec>.*?)</td>.*?'
+        r'<td data-label="(m||M)entor">(?P<Mentor>.*?)</td>.*?'
+        r'<td data-label="(š||Š)ola">(?P<Šola>.*?)</td>.*?'
+        r'<td data-label="(t||T)očke">(?P<Točke>.*?)</td>.*?'
+        r'<td data-label="(m||M)esto">(?P<Mesto>.*?)</td>',
         re.DOTALL)
     najdbe = vzorec.search(blok)
     # print(najdbe['Skupina'])
@@ -38,4 +38,4 @@ def izlusci_podatke_iz_posameznega_bloka(blok):
 
 
 
-# izlusci_podatke_iz_posameznega_bloka((izlusci_podatke(prenesi_html("https://zotks.si/priznanja/logika/")))[0])
+# print(izlusci_podatke_iz_posameznega_bloka((izlusci_podatke(prenesi_html("https://zotks.si/priznanja/logika/")))[0]))
