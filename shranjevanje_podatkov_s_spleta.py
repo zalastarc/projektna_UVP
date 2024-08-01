db914d3447a47bd81f63a6229ffe2b9ed6a50e92
Zala Starc <zala.starc@gmail.com>
2024-08-01 22:58:59 +0200

    Naredila sem csv datoteko.

---
+++ shranjevanje_podatkov_s_spleta.py
@@ -0,0 +1,33 @@
+import csv
+
+from podatki_s_spleta import prenesi_html, izlusci_podatke, izlusci_podatke_iz_posameznega_bloka
+
+def naredi_csv(datoteka):
+    with open(datoteka, 'w', encoding='utf8') as f:
+        pisec = csv.writer(f)
+        pisec.writerow(
+            [
+                'Skupina',
+                'Tekmovalec',
+                'Mentor',
+                'Šola',
+                'Točke',
+                'Mesto',
+            ]
+        )
+        html = prenesi_html('https://zotks.si/priznanja/logika/')
+        bloki = izlusci_podatke(html)
+        for blok in bloki:
+            slovar = izlusci_podatke_iz_posameznega_bloka(blok)
+            pisec.writerow(
+                [
+                    slovar['Skupina'],
+                    slovar['Tekmovalec'],
+                    slovar['Mentor'],
+                    slovar['Šola'],
+                    slovar['Točke'],
+                    slovar['Mesto'],
+                ]
+            )
+
+naredi_csv('datoteka.csv')
