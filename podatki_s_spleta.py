0abe5516d1420a0fc85b1824a12c9a324eac311d
Zala Starc <zala.starc@gmail.com>
2024-08-05 21:53:26 +0200

    Estetski popravki.

--- podatki_s_spleta.py
+++ podatki_s_spleta.py
@@ -2,21 +2,22 @@ import re
 import requests
 
 def prenesi_html(link):
+    """Funkcija, ki prenese html iz spletne strani, katere link podamo v argument."""
     vzorec = requests.get(link)
-    #with open('html', 'w', encoding='utf8') as f:
-    #    print(vzorec.text, file=f)
+    with open('html', 'w', encoding='utf8') as f:
+        print(vzorec.text, file=f)
     return vzorec.text
 
 # prenesi_html("https://zotks.si/priznanja/logika/")
 
-
 def izlusci_podatke(niz):
+    """Funkcija, ki iz html izlušči bloke."""
     vzorec = r'<td data-label="Skupina">.*?</tr>'
     return re.findall(vzorec, niz, flags=re.DOTALL)
 
-# print(len(izlusci_podatke(prenesi_html("https://zotks.si/priznanja/logika/"))))
 
 def izlusci_podatke_iz_posameznega_bloka(blok):
+    """Funkcija, ki iz posameznega bloka izlušči podatke."""
     slovar = {}
     vzorec = re.compile(
         r'<td data-label="(s||S)kupina">(?P<Skupina>.*?)</td>.*?'
@@ -35,7 +36,5 @@ def izlusci_podatke_iz_posameznega_bloka(blok):
     slovar['Točke'] = najdbe['Točke']
     slovar['Mesto'] = najdbe['Mesto']
     return slovar
-
-
 
 # print(izlusci_podatke_iz_posameznega_bloka((izlusci_podatke(prenesi_html("https://zotks.si/priznanja/logika/")))[0]))
